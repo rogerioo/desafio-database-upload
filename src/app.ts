93f4f88ce72bdb31a635cdbfc8bdefcc611a772c
Rogério Júnior <junior.rogerio8@gmail.com>
2020-08-03 18:40:30 -0300

    Add cors and change route list return

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
+import 'express-async-errors';
 import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -11,6 +12,8 @@ import createConnection from './database';
 
 createConnection();
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
